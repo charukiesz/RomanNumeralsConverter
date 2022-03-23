297303605d5702bd055f18ab5c1a59ed5eec9c03
Charulata Kieszkiewicz <charulatapatel2002@yahoo.co.uk>
2022-03-23 19:19:34 +0000

    Tests added; checkRomanNumeralI checkRomanNumeralV checkNotARomanNumeralLowerCasei checkNotARomanNumeralLowerCasev checkNotARomanNumeralLowercaseLetter checkNotARomanNumeralUpperCaseLetter

--- src/test/java/RomanNumeralsConverterTest.java
+++ src/test/java/RomanNumeralsConverterTest.java
@@ -7,28 +7,78 @@ import java.util.Map;
 
 
 public class RomanNumeralsConverterTest {
-
-
     @Test
    public void checkRomanNumeralI(){
-        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+        //Arrange
+         RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
          String romanNumeral = "I";
          Integer expectedResult = 1;
+         //Act
          Integer actualResult = RomanNumeralsConverter.getRomanNumeral(romanNumeral);
-
+        //Assert
          Assertions.assertEquals(expectedResult, actualResult);
     }
    @Test
-    public void checkNotARomanNumeralLowerCase(){
-
+    public void checkRomanNumeralV(){
+       //Arrange
        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
-       String romanNumeral = "i";
-       Integer expectedResult = 1;
+       String romanNumeral = "V";
+       Integer expectedResult = 5;
+       //Act
        Integer actualResult = RomanNumeralsConverter.getRomanNumeral(romanNumeral);
-
+       //Assert
        Assertions.assertEquals(expectedResult, actualResult);
 
    }
 
+   @Test
+    public void checkNotARomanNumeralLowerCasei(){
+       //Arrange
+       RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+       String romanNumeral = "i";
+       Integer expectedResult = null;
+       //Act
+       Integer actualResult = RomanNumeralsConverter.getRomanNumeral(romanNumeral);
+       //Assert
+       Assertions.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void checkNotARomanNumeralLowerCasev(){
+        //Arrange
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+        String romanNumeral = "v";
+        Integer expectedResult = null;
+        //Act
+        Integer actualResult = RomanNumeralsConverter.getRomanNumeral(romanNumeral);
+        //Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void checkNotARomanNumeralLowerCaseLetter() {
+        //Arrange
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+        String romanNumeral = "a";
+        Integer expectedResult = null;
+        //Act
+        Integer actualResult = RomanNumeralsConverter.getRomanNumeral(romanNumeral);
+        //Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void checkNotARomanNumeralUpperCaseLetter(){
+        //Arrange
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+        String romanNumeral = "C";
+        Integer expectedResult = null;
+        //Act
+        Integer actualResult = RomanNumeralsConverter.getRomanNumeral(romanNumeral);
+        //Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+}
 
 }
